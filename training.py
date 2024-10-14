eaef9b41662b7ec1d109eac8e895d9c0acb4c1f7
Areebkhan02 <areeb.adnan.khan@gmail.com>
2024-10-14 18:04:51 +0500

    training loop added

--- training.py
+++ training.py
@@ -1,7 +1,10 @@
 import ultralytics
 from ultralytics import YOLO
 
-model = YOLO("yolo11l.pt")
+#model = YOLO("yolo11l.pt")
 
-result=model.train(data = "/home/areebadnan/Areeb_code/work/Atheritia/Datasets/17_logo_dataset_for_head_training/data.yaml", epochs = 200, imgsz = 640, batch = -1, project = "/home/areebadnan/Areeb_code/work/Atheritia/Datasets/17_logo_dataset_for_head_training/train_iterations/", name = "yolov11l_17logo_train_fullfinetune_cocobase_200epochs")
+#result=model.train(data = "/home/areebadnan/Areeb_code/work/Atheritia/Datasets/17_logo_dataset_for_head_training/data.yaml", epochs = 200, imgsz = 640, batch = -1, project = "/home/areebadnan/Areeb_code/work/Atheritia/Datasets/17_logo_dataset_for_head_training/train_iterations/", name = "yolov11l_17logo_train_fullfinetune_cocobase_200epochs")
 
+model = YOLO("/home/areebadnan/Areeb_code/work/Atheritia/Datasets/17_logo_dataset_for_head_training/train_iterations/yolov11l_17logo_train_fullfinetune_cocobase_200epochs/weights/last.pt")
+
+result = model.train(resume = True)
